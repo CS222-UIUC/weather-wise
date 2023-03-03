d5f29e0c71cd8bc70ecb384221f9f71ceae9c0f3
Eeshan Garr <eeshangarr@gmail.com>
2023-03-03 15:29:51 -0600

    Backend Data Retrieval

---
+++ backend/weather.py
@@ -0,0 +1,16 @@
+import requests
+
+URL = 'https://api.weather.gov/zones/forecast/MNZ060/forecast' # Minneapolis, Minnesota
+
+response = requests.get(URL)
+forecast = response.json()['properties']
+
+print('Forecast updated:', forecast['updated'])
+print('')
+
+for period in forecast['periods']:
+    print(period['name'])
+    print(period['detailedForecast'])
+    print('')
+
+# Run: py .\backend\weather.py in Terminal
