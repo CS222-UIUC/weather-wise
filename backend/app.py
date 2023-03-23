ec75bae837685d1a865198ddb5abd7c15ce8277c
Eeshan Garr <eeshangarr@gmail.com>
2023-03-23 09:58:23 -0500

    Added in backend data parsing algorithms, and created a route for communication between the frontend and backend.

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,24 @@
+import requests
 from flask import Flask
+
+def parse(forecast):
+    parsed = []
+    for period in forecast['periods']:
+        if period['name'] == 'Tonight':
+            parsed.append(period['name'])
+            parsed.append(period['detailedForecast'])
+        if period['name'] == 'Rest Of Today':
+            parsed.append(period['name'])
+            parsed.append(period['detailedForecast'])
+    return parsed
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET"])
-def welcome():
-    return "Hello World!"
-
+def weather(url):
+    response = requests.get(url)
+    parse(response.json()['properties'])
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001)
