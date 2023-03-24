65fec5b488fccd11105d636e3fe27901c0bdfe2b
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-03-24 16:17:02 -0500

    Port template to this branch
    Move's Eli's branch to this one
    
    Co-authored-by: Skywalker31415 <eli.gidron@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,43 @@ function App() {
                 <h1 className="App-title">{APP_NAME}</h1>
             </header>
             <div className="App-content">
-                <p>Some info here</p>
+                <div className="App-columns">
+                    <div className="App-column">
+                        <input
+                            className="Weather-search"
+                            placeholder="Search for weather at..."
+                        ></input>
+                        <div className="Weather-graphs">
+                            <h1>Graph Title</h1>
+                            <p>In theory, a graph. Instead, some cats.</p>
+                            <img src="res/cats.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="App-column">
+                        <div className="Weather-current Weather-forecast-panel">
+                            <div className="Weather-current-column">
+                                <div className="Weather-current-row">70°</div>
+                                <div className="Weather-current-row">
+                                    H:74°, L:65°
+                                </div>
+                            </div>
+                            <div className="Weather-current-column">
+                                Other current weather information here
+                            </div>
+                        </div>
+                        <div className="Weather-hourly Weather-forecast-panel">
+                            Insert forecast by the hour
+                        </div>
+                        <div className="Weather-daily Weather-forecast-panel">
+                            Insert forecast by the day
+                        </div>
+                    </div>
+                </div>
+                <div className="Weather-warnings">
+                    <h1>Warnings and Advice</h1>
+                    <p>This is a great warning</p>
+                    <p>This is a great advice</p>
+                </div>
             </div>
             <footer className="App-footer">
                 <p>
