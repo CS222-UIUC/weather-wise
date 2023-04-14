00f6ad90cacac39d1413abf0260cde9d4e7258eb
eeshangarr <eeshangarr@gmail.com>
2023-04-14 09:54:54 -0500

    Backend codebase completed

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+import requests
+from flask import Flask, jsonify
+
+def parse(forecast):
+    parsed = []
+    for period in forecast['periods']:
+        if period['name'] == 'Tonight':
+            parsed.append(period['name'])
+            parsed.append(period['detailedForecast'])
+        if period['name'] == 'Rest Of Today':
+            parsed.append(period['name'])
+            parsed.append(period['detailedForecast'])
+    return parsed
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET"])
-def welcome():
-    return "Hello World!"
-
+@app.route("/", methods = ["GET"])
+def weather(url):
+    response = requests.get(url)
+    forecast = parse(response.json()['properties'])
+    return jsonify(forecast)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001)
