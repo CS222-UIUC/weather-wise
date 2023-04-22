fae0cb0c6c50107751731ffeefe90341a397296d
Eeshan Garr <87236697+eeshangarr@users.noreply.github.com>
2023-04-21 19:38:25 -0500

    Rewrote the entire backend (#19)
    * Backend Data Retrieval
    
    * Backend Data Retrieval
    
    * Added in backend data parsing algorithms, and created a route for communication between the frontend and backend.
    
    * Used jsonify from flask to display weather data for frontend integration.
    
    * Added in separate test case file, finalizing that the backend codebase works as expected, and will work on connecting Python to JavaScript.
    
    * Backend codebase completed
    
    * Backend codebase completed
    
    * Backend codebase completed
    
    * Remove __pycache__
    
    * Add __pycache__ to .gitignore
    
    * Rewrote parsing algorithm and fixed inputs for Flask route
    
    * Rewrote parsing algorithm and fixed inputs for Flask route
    
    * Rewrote the entire backend
    
    ---------
    
    Co-authored-by: Eeshan Garr <eeshangarr@gmail.com>
    Co-authored-by: Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>

---
+++ backend/test-app.py
@@ -0,0 +1,44 @@
+import pytest
+import requests
+import numpy as np
+from app import *
+
+# parse(forecast)
+
+# def testParse(url):
+    # if parse(url) == ['Rest Of Today', 'Mostly cloudy late in the morning then becoming mostly sunny. Highs in the mid 30s. Northwest winds 5 to 10 mph.', 'Tonight', 'Mostly clear. Areas of fog after midnight. Lows around 16. Northwest winds up to 5 mph.']: # Output
+        # print("🎉")
+    # else:
+        # print("😭")
+    # if parse(url) != [' ']: # Empty
+        # print("🎉")
+    # else:
+        # print("😭")
+    # return 
+
+# url = 'https://api.weather.gov/zones/forecast/MNZ060/forecast' 
+# response = requests.get(url)
+# testParse(response.json()['properties'])
+
+# weather(url)
+
+# def testWeather(url):
+    # if (weather(url) == "Hello, World!"): # Response
+        # print("🎉")
+    # else:
+        # print("😭")
+    # return    
+
+# url = 'https://api.weather.gov/zones/forecast/MNZ060/forecast' 
+# testWeather(url)
+
+# jsonify(forecast)
+
+# def testJsonify(url):
+    # if weather(url) != None:
+        # print("🎉")
+    # else:
+        # print("😭")
+
+# url = "https://api.weather.gov/zones/forecast/MNZ060/forecast"
+# testJsonify(url)
