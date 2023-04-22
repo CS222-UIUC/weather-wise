118507c7f869ed4102cad27a8fc3f7e05a2e61d1
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-04-21 23:13:29 -0500

    Add formatting

--- backend/app.py
+++ backend/app.py
@@ -33,24 +33,30 @@ def weather(location):
     lowTemperature = ""
     for period in highLowResponse.json()["properties"]["periods"]:
         if period["number"] == 1:
-            highTemperature = str(period["temperature"]) 
-            otherWeatherInformation = str(period["detailedForecast"]) # Other weather information
+            highTemperature = str(period["temperature"])
+            otherWeatherInformation = str(
+                period["detailedForecast"]
+            )  # Other weather information
         if period["number"] == 2:
             lowTemperature = str(period["temperature"])
     # Current temperature
-    currentResponse = requests.get(str(baseResponse.json()["properties"]["forecastHourly"]))
+    currentResponse = requests.get(
+        str(baseResponse.json()["properties"]["forecastHourly"])
+    )
     currentTemperature = ""
-    tz = pytz.timezone('America/New_York') 
+    tz = pytz.timezone("America/New_York")
     now = datetime.now(tz)
-    currentDateTime = now.strftime('%Y-%m-%dT%H:%M:%S%z')
-    currentDateTime = currentDateTime[:-2] + ':' + currentDateTime[-2:]
+    currentDateTime = now.strftime("%Y-%m-%dT%H:%M:%S%z")
+    currentDateTime = currentDateTime[:-2] + ":" + currentDateTime[-2:]
     currentDateTime = currentDateTime[:-11] + "00:00-05:00"
     for period in currentResponse.json()["properties"]["periods"]:
         if currentDateTime == str(period["startTime"]):
             currentTemperature = period["temperature"]
             break
     # Hourly temperature
-    hourlyResponse = requests.get(str(baseResponse.json()["properties"]["forecastHourly"]))
+    hourlyResponse = requests.get(
+        str(baseResponse.json()["properties"]["forecastHourly"])
+    )
     hourlyTemperatures = []
     for period in hourlyResponse.json()["properties"]["periods"]:
         if period["number"] <= 6:
@@ -59,7 +65,7 @@ def weather(location):
     rounded = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
     futureHours = []
     for time in range(1, 7):
-        hour = (rounded + timedelta(hours = time)).strftime("%I:%M %p")
+        hour = (rounded + timedelta(hours=time)).strftime("%I:%M %p")
         futureHours.append(hour)
     # Daily temperature
     days = []
@@ -72,27 +78,28 @@ def weather(location):
     print(days)
     print(dailyTemperatures)
     dictionary = {
-        "current" : currentTemperature,
-        "other" : otherWeatherInformation,
-        "today": {"high": highTemperature , "low" : lowTemperature},
+        "current": currentTemperature,
+        "other": otherWeatherInformation,
+        "today": {"high": highTemperature, "low": lowTemperature},
         "hourly": {
-            "0":  {"time": futureHours[0], "weather": hourlyTemperatures[0]},
-            "1":  {"time": futureHours[1], "weather": hourlyTemperatures[1]},
-            "2":  {"time": futureHours[2], "weather": hourlyTemperatures[2]},
-            "3":  {"time": futureHours[3], "weather": hourlyTemperatures[3]},
-            "4":  {"time": futureHours[4], "weather": hourlyTemperatures[4]},
-            "5":  {"time": futureHours[5], "weather": hourlyTemperatures[5]},
+            "0": {"time": futureHours[0], "weather": hourlyTemperatures[0]},
+            "1": {"time": futureHours[1], "weather": hourlyTemperatures[1]},
+            "2": {"time": futureHours[2], "weather": hourlyTemperatures[2]},
+            "3": {"time": futureHours[3], "weather": hourlyTemperatures[3]},
+            "4": {"time": futureHours[4], "weather": hourlyTemperatures[4]},
+            "5": {"time": futureHours[5], "weather": hourlyTemperatures[5]},
         },
         "daily": {
-            "0":  {"day": days[0], "weather": dailyTemperatures[0]},
-            "1":  {"day": days[1], "weather": dailyTemperatures[1]},
-            "2":  {"day": days[2], "weather": dailyTemperatures[2]},
-            "3":  {"day": days[3], "weather": dailyTemperatures[3]},
-            "4":  {"day": days[4], "weather": dailyTemperatures[4]},
-            "5":  {"day": days[5], "weather": dailyTemperatures[5]},
-        }
+            "0": {"day": days[0], "weather": dailyTemperatures[0]},
+            "1": {"day": days[1], "weather": dailyTemperatures[1]},
+            "2": {"day": days[2], "weather": dailyTemperatures[2]},
+            "3": {"day": days[3], "weather": dailyTemperatures[3]},
+            "4": {"day": days[4], "weather": dailyTemperatures[4]},
+            "5": {"day": days[5], "weather": dailyTemperatures[5]},
+        },
     }
-    return jsonify(dictionary) # Return as a JSON
+    return jsonify(dictionary)  # Return as a JSON
 
-if __name__ == "__main__": # Run app
+
+if __name__ == "__main__":  # Run app
     app.run()
