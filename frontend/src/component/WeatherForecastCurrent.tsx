fceaa57db730a3eb337da66644ecd310cd54509f
Skywalker31415 <eli.gidron@gmail.com>
2023-04-21 19:13:56 -0500

    committing the basic stuff I added before merge

--- frontend/src/component/WeatherForecastCurrent.tsx
+++ frontend/src/component/WeatherForecastCurrent.tsx
@@ -1,9 +1,54 @@
 import React, {useState, useEffect} from 'react'
 
 import WeatherPanel from './WeatherPanel'
+import icons from './WeatherIcon.module.sass'
+import styles from './WeatherForecastCurrent.module.sass'
 
-import './WeatherForecastCurrent.css'
+const CLOUDY = "images/cloudy.png"
+const PARTLY_CLOUDY = "images/partly_cloudy.png"
+const RAINY = "images/rainy.png"
+const SNOWY = "images/snowy.png"
+const STORMY = "images/stormy.png"
+const SUNNY = "images/sunny.png"
+class CurrentWeather {
+    current_temp: number
+    high: number
+    low: number
+    current_weather: string
+    constructor()   {
+        //call api, possibly pass in?
+        this.current_temp = 10000
+        this.high = 78
+        this.low = 68
+        this.current_weather = "snowy"
+    }
 
+    getImage()  {
+        if (this.current_weather === "cloudy")
+            return (<img src={CLOUDY}></img>)
+        if (this.current_weather === "partly_cloudy")
+            return (<img src={PARTLY_CLOUDY}></img>)
+        if (this.current_weather === "rainy")
+            return (<img src={RAINY}></img>)
+        if (this.current_weather === "snowy")
+            return (<img src={SNOWY}></img>)
+        if (this.current_weather === "stormy")
+            return (<img src={STORMY}></img>)
+        if (this.current_weather === "sunny")
+            return (<img src={SUNNY}></img>)
+    }
+    getCurrent()    {
+        return this.current_temp
+    }
+    getHigh(){
+        return this.high
+    }
+    getLow()    {
+        return this.low
+    }
+    
+    
+}
 export default function WeatherForecastCurrent() {
     const [data, setdata] = useState([{}])
     useEffect( () => {
@@ -18,11 +63,15 @@ export default function WeatherForecastCurrent() {
         )
     }, [])
     console.log(typeof(data))
+    const weather = new CurrentWeather()
     return (
-        <WeatherPanel className="WeatherForecastCurrent">
-            <div className="WeatherForecastCurrent-column">
-                <div className="WeatherForecastCurrent-row">70°</div>
-                <div className="WeatherForecastCurrent-row">H:74°, L:65°</div>
+        <WeatherPanel className={styles.WeatherForecastCurrent}>
+            <div className={styles.column}>
+                <div className={styles.row}>{weather.getCurrent()}°</div>
+                <div className={styles.row}>H:{weather.getHigh()}°, L:{weather.getLow()}°</div>
+            </div>
+            <div className={icons.WeatherIcon}>
+                {weather.getImage()}
             </div>
             <div className="WeatherForecastCurrent-column">
                 {typeof(data) !== 'string' ? (
