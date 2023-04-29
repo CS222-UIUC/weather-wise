2b9f63aef19681918bf0cd0e1875f6c384cb55a6
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-04-29 00:46:34 -0500

    Improve weekly forecast styling

--- frontend/src/component/WeatherForecastDaily.tsx
+++ frontend/src/component/WeatherForecastDaily.tsx
@@ -4,10 +4,29 @@ import WeatherPanel from './WeatherPanel'
 
 import styles from './WeatherForecastDaily.module.sass'
 import { DayForecast } from 'src/data/WeatherReport'
+import { GetImageSrcFor } from 'src/data/WeatherType'
 
 type MyProps = {
     unit: string
     forecast: Array<DayForecast>
+}
+
+const DAYS = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+]
+
+function formatDay(day: string) {
+    if (!DAYS.includes(day.toLowerCase())) {
+        day = DAYS[new Date().getDay()]
+    }
+
+    return day.toUpperCase().substring(0, 3)
 }
 
 export default class WeatherForecastDaily extends React.Component<MyProps> {
@@ -15,10 +34,16 @@ export default class WeatherForecastDaily extends React.Component<MyProps> {
         return (
             <WeatherPanel className={styles.WeatherForecastDaily}>
                 {this.props.forecast.map((dayForecast, i) => (
-                    <p key={i}>
-                        {dayForecast.day}: {dayForecast.high}°{' / '}
-                        {dayForecast.low}° | {dayForecast.shortForecast}
-                    </p>
+                    <div key={i}>
+                        <p>{formatDay(dayForecast.day)}</p>
+                        <p>
+                            {dayForecast.high}°{' / '}
+                            {dayForecast.low}°
+                        </p>
+                        <img
+                            src={GetImageSrcFor(dayForecast.shortForecast)}
+                        ></img>
+                    </div>
                 ))}
             </WeatherPanel>
         )
