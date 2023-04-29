6f7a97e68d8894c5a00254c86d304318006dc99e
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-04-28 23:50:19 -0500

    Fix formatting of warnings

--- frontend/src/component/WeatherWarnings.tsx
+++ frontend/src/component/WeatherWarnings.tsx
@@ -2,19 +2,29 @@ import React from 'react'
 
 import styles from './WeatherWarnings.module.sass'
 
-type MyProps = { forecast?: string }
+type MyProps = { forecast: string }
 
 export default class WeatherWarnings extends React.Component<MyProps> {
     render() {
+        let formattedForecast = this.props.forecast
+            .trim()
+            .replaceAll('..', '.')
+            .replaceAll('..', '.')
+
+        if (formattedForecast.endsWith('.')) {
+            formattedForecast = formattedForecast.substring(
+                0,
+                formattedForecast.length - 1
+            )
+        }
+
         return (
             <div className={styles.WeatherWarnings}>
                 <h1>Warnings and Advice</h1>
                 <ul>
-                    {this.props.forecast &&
-                        this.props.forecast
-                            .trim()
-                            .split('. ')
-                            .map((warning, i) => <li key={i}>{warning}.</li>)}
+                    {formattedForecast.split('. ').map((warning, i) => (
+                        <li key={i}>{warning}</li>
+                    ))}
                 </ul>
             </div>
         )
